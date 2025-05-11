b79f238f37d2271c16d47b040c7bcf022059e2ca
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-05-11 08:35:35 +0530

    refactor(bunny): storage stream cdn embed and transcript url

--- constants/index.ts
+++ constants/index.ts
@@ -54,10 +54,10 @@ export const MAX_THUMBNAIL_SIZE = 10 * 1024 * 1024;
 
 export const BUNNY = {
   STREAM_BASE_URL: "https://video.bunnycdn.com/library",
-  STORAGE_BASE_URL: "https://sg.storage.bunnycdn.com/snapcast",
-  CDN_URL: "https://snapcast.b-cdn.net",
+  STORAGE_BASE_URL: "https://storage.bunnycdn.com/sikku-snapcast",
+  CDN_URL: "https://sikku-snapcast.b-cdn.net",
   EMBED_URL: "https://iframe.mediadelivery.net/embed",
-  TRANSCRIPT_URL: "https://vz-47a08e64-84d.b-cdn.net",
+  TRANSCRIPT_URL: "https://vz-5a87753d-ae7.b-cdn.net",
 };
 
 export const emojis = ["😂", "😍", "👍"];
