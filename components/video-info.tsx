2ef81484d70fc687692fa3a79da35a94d0c6736d
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-05-11 09:09:01 +0530

    feat(video): add video details page with header info and player

---
+++ components/video-info.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { useState } from "react";
+import { infos } from "~/constants";
+import { cn, parseTranscript } from "~/lib/utils";
+import EmptyState from "~/components/empty-state";
+
+const VideoInfo = ({
+  transcript,
+  createdAt,
+  description,
+  videoId,
+  videoUrl,
+  title,
+}: VideoInfoProps) => {
+  const [info, setInfo] = useState("transcript");
+  const parsedTranscript = parseTranscript(transcript || "");
+
+  const renderTranscript = () => (
+    <ul className="transcript">
+      {parsedTranscript.length > 0 ? (
+        parsedTranscript.map((item, index) => (
+          <li key={index}>
+            <h2>[{item.time}]</h2>
+            <p>{item.text}</p>
+          </li>
+        ))
+      ) : (
+        <EmptyState
+          icon="/assets/icons/copy.svg"
+          title="No transcript available"
+          description="This video doesn’t include any transcribed content!"
+        />
+      )}
+    </ul>
+  );
+
+  const metaData = [
+    {
+      label: "Video title",
+      value: `${title} - ${new Date(createdAt).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`,
+    },
+    {
+      label: "Video description",
+      value: description,
+    },
+    {
+      label: "Video id",
+      value: videoId,
+    },
+    {
+      label: "Video url",
+      value: videoUrl,
+    },
+  ];
+
+  const renderMetadata = () => (
+    <div className="metadata">
+      {metaData.map(({ label, value }, index) => (
+        <article key={index}>
+          <h2>{label}</h2>
+          <p
+            className={cn({
+              "text-pink-100 truncate": label === "Video url",
+            })}
+          >
+            {value}
+          </p>
+        </article>
+      ))}
+    </div>
+  );
+
+  return (
+    <section className="video-info">
+      <nav>
+        {infos.map((item) => (
+          <button
+            key={item}
+            className={cn({
+              "text-pink-100 border-b-2 border-pink-100": info === item,
+            })}
+            onClick={() => setInfo(item)}
+          >
+            {item}
+          </button>
+        ))}
+      </nav>
+      {info === "transcript" ? renderTranscript() : renderMetadata()}
+    </section>
+  );
+};
+
+export default VideoInfo;
